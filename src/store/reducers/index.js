be5c28a016732f3837fbc137b8b289d94e8ecfce
eric <ericsilva328@gmail.com>
2020-08-12 19:42:59 -0300

    Finalizado

---
+++ src/store/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux';
+
+import course from './course';
+
+export default combineReducers({
+    course,
+});
+
+
+//{ 
+//    course : { modules: [], activeLesson:{}, activeModule:{},}
+//    users: {name'', , avatar:''}
+//}
